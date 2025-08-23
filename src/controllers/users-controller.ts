5d2ad1f06d67f7f41b3148460d3e7e27ba310771
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-23 15:38:57 -0300

    FEAT: Password hash encryption

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+import { hash } from "bcrypt";
 
 class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "OK" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    const hashedPassword = await hash(password, 8);
+    return response.json({ message: "OK", hashedPassword });
   }
 }
 
