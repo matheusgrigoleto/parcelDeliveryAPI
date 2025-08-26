322c526aa9b2f2aa5c5644b9d9090ea2c2c0b9c3
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-26 16:48:53 -0300

    TEST: Deleted register of user

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,17 +1,26 @@
-import request from "supertest"
+import request from "supertest";
+import { prisma } from "@/database/prisma";
 
 import { app } from "@/app";
 
 describe("UserController", () => {
+  let user_id: string;
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } });
+  });
+
   it("should create a new user sucessfully", async () => {
     const response = await request(app).post("/users").send({
       name: "Test User",
       email: "testuser@example.com",
-      password: "password123"
-    })
+      password: "password123",
+    });
 
-    expect(response.status).toBe(201)
-    expect(response.body).toHaveProperty("id")
-    expect(response.body.name).toBe("Test User")
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.name).toBe("Test User");
+
+    user_id = response.body.id;
   });
 });
