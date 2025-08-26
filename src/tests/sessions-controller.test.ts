113883961042a7e6e345eae30c144a54370a6b9f
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-26 17:07:37 -0300

    TEST: Remove user of authentication

--- src/tests/sessions-controller.test.ts
+++ src/tests/sessions-controller.test.ts
@@ -1,9 +1,14 @@
 import request from "supertest";
+import { prisma } from "@/database/prisma";
 
 import { app } from "@/app";
 
 describe("SessionsController", () => {
   let user_id: string;
+
+   afterAll(async () => {
+      await prisma.user.delete({ where: { id: user_id } });
+    });
 
   it("should authenticate a and get access token", async () => {
     const userResponse = await request(app).post("/users").send({
