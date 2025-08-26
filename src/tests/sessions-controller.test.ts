3cc97763c565f9a13930ebd3319123e85b5560e2
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-26 17:05:47 -0300

    TEST: Authentication

---
+++ src/tests/sessions-controller.test.ts
@@ -0,0 +1,25 @@
+import request from "supertest";
+
+import { app } from "@/app";
+
+describe("SessionsController", () => {
+  let user_id: string;
+
+  it("should authenticate a and get access token", async () => {
+    const userResponse = await request(app).post("/users").send({
+      name: "Auth test User",
+      email: "auth_test_user@example.com",
+      password: "password123",
+    });
+
+    user_id = userResponse.body.id;
+
+    const sessionResponse = await request(app).post("/sessions").send({
+      email: "auth_test_user@example.com",
+      password: "password123",
+    });
+
+    expect(sessionResponse.status).toBe(200)
+    expect(sessionResponse.body.token).toEqual(expect.any(String))
+  });
+});
